6dc90484de263cd35fa579911e277056edae7254
eikonoklastess <m.w.outaleb@gmail.com>
2024-08-28 17:33:47 -0400

    tests

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,21 @@
+package auth
+
+import (
+	"net/http"
+	"reflect"
+	"testing"
+)
+
+func TestGetAPIKey(t *testing.T) {
+	header := http.Header{
+		"Authorization": []string{"ApiKey YWxhZGRpbjpvcGVuc2VzYW1l"},
+	}
+	got, err := GetAPIKey(header)
+	if err != nil {
+		t.Fatalf("Error: %v", err)
+	}
+	want := "YWxhZGRpbjpvcGVuc2VzYW1"
+	if !reflect.DeepEqual(want, got) {
+		t.Fatalf("expected: %v, got: %v", want, got)
+	}
+}
